5cd292f298cb095d6c0a587d53aedbe5d410fad4
Tmarra122333 <Tmarra122333@gmail.com>
2022-02-14 10:20:10 -0500

    finchComplete

---
+++ db/create_guitar.sql
@@ -0,0 +1,24 @@
+INSERT INTO main_app_guitar(name, img, bio, verified_guitar, created_at)
+VALUES
+    ('Taylor 214ce','https://media.guitarcenter.com/is/image/MMGS7/L69529000001000-00-1600x1600.jpg','Taylor 200 series sweetheart guitar.  Mid Level but plays like high end.','t','2021-05-07 18:57:42.517205-07');
+
+INSERT INTO main_app_guitar(name, img, bio, verified_guitar, created_at)
+VALUES
+    ('Taylor K24ce','https://i.ebayimg.com/images/g/7fYAAOSwq19XBhvo/s-l600.jpg','This guitar is made of KOA wood.  You can only harvest this type of Koa wood when a tree naturally falls on its own.  It originates and only comes from Hawaii. Best guitar on earth','f','2021-05-07 18:57:42.517205-07');
+
+INSERT INTO main_app_guitar(name, img, bio, verified_guitar, created_at)
+VALUES
+    ('Gibson Hummingbird','https://max.guitars/media/catalog/product/cache/72269915de88ed6ece6209277c94ce43/m/a/maxguitar_art-12675_gibson_hummingbird-1.jpg','Beauty at its finest.  Gibson Hummingbirds have everything you could possibly want or need, all the way to the aesthetic eye candy of its beautiful design.','f','2021-05-07 18:57:42.517205-07');
+
+INSERT INTO main_app_guitar(name, img, bio, verified_guitar, created_at)
+VALUES
+    ('Washburn D100 Dreadnought','https://upload.wikimedia.org/wikipedia/commons/d/d1/Washburn_D100DL.jpg','A man named Max McGrath played this guitar to its fame.  Known for his hammering and pulling technique Max Mcgrath made this guitar the novelty of bluegrass music','t','2021-05-07 18:57:42.517205-07');
+
+INSERT INTO main_app_guitar(name, img, bio, verified_guitar, created_at)
+VALUES
+    ('Martin D28','https://www.martinguitar.com/on/demandware.static/-/Sites-martin-master-catalog/default/dw77e28aa4/images/D-28/D-28_f.jpg','This 1970s guitar has everything you need.  A big boomy sound and is a favorite played by Neil Young','t','2021-05-07 18:57:42.517205-07');
+
+INSERT INTO main_app_guitar(name, img, bio, verified_guitar, created_at)
+VALUES
+    ('Ovation Celebrity','https://media.sweetwater.com/api/i/q-82__ha-b8038e96949a29b7__hmac-cd8ead02e09c02602c236ea52c175ddd951e4a83/images/items/750/CE48PRG-large.jpg','This guitar is so sweet it has an open hollow body that gives the sensation of vibrational sounds unlike any other!','t','2021-05-07 18:57:42.517205-07');
+
--- main_app/urls.py
+++ main_app/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('', views.Home.as_view(), name="home"), # <- here we have added the new path
     path('about/', views.About.as_view(), name="about"),
     path('guitars/', views.GuitarList.as_view(), name="guitar_list"),
+    path('guitars/new/', views.GuitarCreate.as_view(), name="guitar_create"),
+    path('guitars/<int:pk>/', views.GuitarDetail.as_view(), name="guitar_detail"),
+    path('guitars/<int:pk>/update',views.GuitarUpdate.as_view(), name="guitar_update"),
+    path('guitars/<int:pk>/delete',views.GuitarDelete.as_view(), name="guitar_delete"),
+    
 ]
